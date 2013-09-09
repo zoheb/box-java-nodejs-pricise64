2e3b5848909ebb2947fb734c16f83338b323c9b0
Zoheb Sait <zoheb.sait@gmail.com>
2013-09-09 11:45:00 -0400

    fix

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo apt-get install unzip -y -q
 echo "********"
 if [ ! -d "/home/vagrant/installs/h2" ]; then
 
-	if [! -f "/tmp/h2.zip"]; then
+	if [ ! -f "/tmp/h2.zip"]; then
 		curl -o /tmp/h2.zip  http://www.h2database.com/h2-2013-05-25.zip
 	fi
 	su - vagrant -c "cp /tmp/h2.zip /home/vagrant/installs/h2.zip; cd /home/vagrant/installs; unzip h2.zip; chmod 777 /home/vagrant/installs/h2/bin/h2.sh"
